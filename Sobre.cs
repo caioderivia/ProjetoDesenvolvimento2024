6e8cf6977d5db974346016290d8e7e5788315f3e
Caio Augusto de Oliveira <caioau2004@gmail.com>
2024-12-02 11:28:11 -0300

    Sobre.cs

---
+++ Sobre.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Windows.Forms;
+
+namespace AgendaProSolutions
+{
+    public partial class Sobre : Form
+    {
+        public Sobre()
+        {
+            InitializeComponent();
+        }
+    }
+}
